38bb8816dc8619b937c7576cd9f29b2c568c80f3
Kaiyu Zheng <kaiyutony@gmail.com>
2021-09-29 12:44:30 -0400

    remove test_local.sh

--- tests/test_local.sh
+++
@@ -1,7 +0,0 @@
-python test_conversion_pomdp-solve.py ~/software/pomdp-solve-5.4/src/pomdp-solve
-
-python test_conversion_pomdpx.py ~/software/sarsop/src/pomdpconvert
-
-python test_vi_pruning.py ~/software/pomdp-solve-5.4/src/pomdp-solve
-
-python test_sarsop.py ~/software/sarsop/src/pomdpsol
