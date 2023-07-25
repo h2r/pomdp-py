04ef7da06c5d143df28b8788d54dd488051663ac
Kaiyu Zheng <kaiyutony@gmail.com>
2023-07-25 03:26:34 -0400

    version bump; sorting MANIFEST.in

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ extensions += [
 
 setup(name='pomdp-py',
       packages=find_packages(),
-      version='1.3.2',
+      version='1.3.3',
       description='Python POMDP Library.',
       long_description=long_description,
       long_description_content_type="text/x-rst",
