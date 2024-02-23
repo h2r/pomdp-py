0200f41cd20c476950717a327fc98d16218001bc
Kaiyu Zheng <kzheng@theaiinstitute.com>
2024-02-23 17:12:24 -0500

    black

--- tests/test_framework_basics.py
+++ tests/test_framework_basics.py
@@ -7,17 +7,15 @@ TRANSITION = pomdp_py.TabularTransitionModel(
         # state, action, next_state
         ("hungry", "eat", "full"): 0.7,
         ("hungry", "eat", "hungry"): 0.3,
-
         ("hungry", "sleep", "full"): 0.01,
         ("hungry", "sleep", "hungry"): 0.99,
-
         ("full", "eat", "full"): 0.9,
         ("full", "eat", "hungry"): 0.1,
-
         ("full", "sleep", "full"): 0.5,
         ("full", "sleep", "hungry"): 0.5,
     }
 )
+
 
 def test_agent_set_model() -> None:
     b0 = pomdp_py.Histogram({"hungry": 0.5, "full": 0.5})
@@ -34,6 +32,7 @@ def test_agent_set_model() -> None:
     agent.set_models(policy_model=policy)
     assert agent.policy_model.sample(b0.random()) in ["eat", "sleep"]
 
+
 def test_env_set_model() -> None:
     # test that agent can be created with incomplete models
     # and we can set the agent's model after its creation
@@ -46,6 +45,7 @@ def test_env_set_model() -> None:
 def run() -> None:
     test_agent_set_model()
     test_env_set_model()
+
 
 if __name__ == "__main__":
     run()
