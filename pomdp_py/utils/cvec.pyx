fa8dac1366499fd048745898cd49b681f72405e9
Troi Williams <40696868+troiwill@users.noreply.github.com>
2024-04-18 18:44:50 -0400

    Added the comments to function calls.

--- pomdp_py/utils/cvec.pyx
+++ pomdp_py/utils/cvec.pyx
@@ -9,45 +9,74 @@ cnp.import_array()
 
 ArrayDtype_t = np.float64
 
+
 cdef inline Arrayf_t null_vector(unsigned int n_zeros):
     return np.zeros((n_zeros,), ArrayDtype_t)
 
 
 cpdef inline Arrayf_t list_to_vectord(list[float] values):
+    """
+    Converts a list of numbers to an underlying array data structure.
+    
+    Args:
+        values (list[float]): The values to place in the array. 
+
+    Returns:
+        An array.
+    """
     return np.array(values, ArrayDtype_t)
 
 
 cpdef inline list[float] vectord_to_list(Arrayf_t values):
+    """
+    Converts an array to a list of float.
+    
+    Args:
+        values (array): The values to place in a list. 
+
+    Returns:
+        A list of float.
+    """
     return values.tolist()
-
-
-cdef inline bint vectors_are_not_same_size(double[:] v0, double[:] v1):
-    return v0.shape[0] != v1.shape[0]
 
 
 cdef inline bint vector_size_is_zero(double[:] v):
     return v.shape[0] == 0
 
 
+cdef inline bint vectors2_are_not_valid(double[:] v0, double[:] v1):
+    return (
+        v0.shape[0] != v1.shape[0]
+        | vector_size_is_zero(v0)
+    )
+
+
+cdef inline bint vectors3_are_not_valid(double[:] v0, double[:] v1, double[:] v2):
+    return (
+        v0.shape[0] != v1.shape[0]
+        | v1.shape[0] != v2.shape[0]
+        | vector_size_is_zero(v0)
+    )
+
+
 cdef double vector_dot_prod(double[:] v0, double[:] v1):
+    if vectors2_are_not_valid(v0, v1):
+        raise ValueError(
+            "All vectors must have the same size and contain at least one value."
+        )
+
     cdef unsigned int i = 0
     cdef double _sum = 0.
-
-    if vectors_are_not_same_size(v0, v1):
-        raise ValueError("Both vectors must have the same size.")
-    if vector_size_is_zero(v0):
-        raise ValueError("Vectors should contain at least one value.")
-
     for i in range(v0.shape[0]):
         _sum += (v0[i] * v1[i])
     return _sum
 
 
 cdef void vector_add(double[:] v0, double[:] v1, double[:] res):
-    if vectors_are_not_same_size(v0, v1):
-        raise ValueError("Both vectors must have the same size.")
-    if vector_size_is_zero(v0):
-        raise ValueError("Vectors should contain at least one value.")
+    if vectors3_are_not_valid(v0, v1, res):
+        raise ValueError(
+            "All vectors must have the same size and contain at least one value."
+        )
 
     cdef unsigned int i = 0
     for i in range(v0.shape[0]):
@@ -55,19 +84,21 @@ cdef void vector_add(double[:] v0, double[:] v1, double[:] res):
 
 
 cdef void vector_adds(double[:] v, double scalar, double[:] res):
-    if vector_size_is_zero(v):
-        raise ValueError("Vector should contain at least one value.")
+    if vectors2_are_not_valid(v, res):
+        raise ValueError(
+            "All vectors must have the same size and contain at least one value."
+        )
 
     cdef unsigned int i = 0
     for i in range(v.shape[0]):
         res[i] = v[i] + scalar
 
 
-cdef void vector_muls(double[:] v, double scalar,double[:] res):
-    if vectors_are_not_same_size(v, res):
-        raise ValueError("Vectors v and res must be the same size.")
-    if vector_size_is_zero(v):
-        raise ValueError("Vector should contain at least one value.")
+cdef void vector_muls(double[:] v, double scalar, double[:] res):
+    if vectors2_are_not_valid(v, res):
+        raise ValueError(
+            "All vectors must have the same size and contain at least one value."
+        )
 
     cdef unsigned int i = 0
     for i in range(v.shape[0]):
@@ -75,10 +106,10 @@ cdef void vector_muls(double[:] v, double scalar,double[:] res):
 
 
 cdef void vector_sub(double[:] v0, double[:] v1, double[:] res):
-    if vectors_are_not_same_size(v0, v1):
-        raise ValueError("Both vectors must have the same size.")
-    if vector_size_is_zero(v0):
-        raise ValueError("Vectors should contain at least one value.")
+    if vectors3_are_not_valid(v0, v1, res):
+        raise ValueError(
+            "All vectors must have the same size and contain at least one value."
+        )
 
     cdef unsigned int i = 0
     for i in range(v0.shape[0]):
@@ -86,8 +117,10 @@ cdef void vector_sub(double[:] v0, double[:] v1, double[:] res):
 
 
 cdef void vector_subvs(double[:] v, double scalar, double[:] res):
-    if vector_size_is_zero(v):
-        raise ValueError("Vector should contain at least one value.")
+    if vectors2_are_not_valid(v, res):
+        raise ValueError(
+            "All vectors must have the same size and contain at least one value."
+        )
 
     cdef unsigned int i = 0
     for i in range(v.shape[0]):
@@ -95,8 +128,10 @@ cdef void vector_subvs(double[:] v, double scalar, double[:] res):
 
 
 cdef void vector_subsv(double scalar, double[:] v, double[:] res):
-    if vector_size_is_zero(v):
-        raise ValueError("Vector should contain at least one value.")
+    if vectors2_are_not_valid(v, res):
+        raise ValueError(
+            "All vectors must have the same size and contain at least one value."
+        )
 
     cdef unsigned int i = 0
     for i in range(v.shape[0]):
@@ -104,10 +139,10 @@ cdef void vector_subsv(double scalar, double[:] v, double[:] res):
 
 
 cdef void vector_scalar_div(double[:] v, double scalar, double[:] res):
-    if vector_size_is_zero(v):
-        raise ValueError("Vector should contain at least one value.")
-    if scalar == 0.0:
-        raise ZeroDivisionError("Scalar division by zero!")
+    if vectors2_are_not_valid(v, res):
+        raise ValueError(
+            "All vectors must have the same size and contain at least one value."
+        )
 
     cdef unsigned int i = 0
     for i in range(v.shape[0]):
@@ -157,17 +192,13 @@ cdef void vector_clip(double[:] v, double min_value, double max_value):
 
 
 cdef void vector_copy(double[:] src, double[:] dst):
-    if vector_size_is_zero(src):
-        raise ValueError("Vector should contain at least one value.")
+    if vectors2_are_not_valid(src, dst):
+        raise ValueError(
+            "All vectors must have the same size and contain at least one value."
+        )
     cdef int i = 0
     for i in range(src.shape[0]):
         dst[i] = src[i]
-
-
-cdef void vector_resize(Arrayf_t v, unsigned int new_size):
-    if new_size <= 0:
-        raise ValueError("New vector size must be a positive integer.")
-    v = np.zeros((new_size,), dtype=v.dtype)
 
 
 cdef class Vector:
@@ -192,11 +223,18 @@ cdef class Vector:
         return vectord_to_list(self._vals)
 
     def as_vector(self) -> np.ndarray:
+        """
+        Returns the internal array of values.
+        """
         return self._vals[:]
 
     cpdef void clip(Vector self, double min_value, double max_value):
         """
         Clips the values within the value using the given min and max values.
+        
+        Args:
+            min_value (double): The minimum value that will be in the vector.
+            max_value (double): The maximum value that will be in the vector.
         """
         vector_clip(self._vals, min_value, max_value)
 
@@ -209,46 +247,112 @@ cdef class Vector:
     cpdef double dot(Vector self, Vector other):
         """
         Performs the dot product between two Vectors.
+        
+        Args:
+            other (Vector): The other vector to perform the dot product with.
+
+        Returns:
+            The dot product of this and the other Vector.
         """
         return vector_dot_prod(self._vals, other._vals)
 
     @staticmethod
     def fill(value: float, n_values: int) -> Vector:
+        """
+        Creates a new Vector and fills it with the given value.
+
+        Args:
+            value (float): The value to fill the Vector with.
+            n_values (int): The number of values to fill the Vector with.
+
+        Returns:
+            A new Vector with n_values value's.
+        """
         return Vector([value] * n_values)
 
     cpdef int len(Vector self):
+        """
+        Returns the length of the Vector.
+        """
         return self._length
 
     cpdef unsigned int argmax(Vector self):
+        """
+        Returns the index of the max value.
+        """
         return vector_argmax(self._vals)
 
     cpdef unsigned int argmin(Vector self):
+        """
+        Returns the index of the min value.
+        """
         return vector_argmin(self._vals)
 
     cpdef double max(Vector self):
+        """
+        Returns the max value.
+        """
         return self._vals[self.argmax()]
 
     cpdef double min(Vector self):
+        """
+        Returns the min value.
+        """
         return self._vals[self.argmin()]
 
     @staticmethod
     def null(n_zeros: int) -> Vector:
+        """
+        Creates a null Vector with N zeros.
+
+        Args:
+            n_zeros (int): The number of zeros to place in the Vector.
+
+        Returns:
+            A Vector with all zeros.
+        """
         return Vector.fill(0.0, n_zeros)
 
     cdef void resize(Vector self, unsigned int new_size):
-        self._vals = null_vector(new_size)
-        self._res_buff = null_vector(new_size)
-        self._length = self._vals.shape[0]
+        """
+        Resizes the Vector with the new specified size.
+        
+        Args:
+            new_size (unsigned int): The new size of the Vector. 
+
+        Returns:
+
+        """
+        if new_size == self._length:
+            self.zeros()
+        else:
+            self._vals = null_vector(new_size)
+            self._res_buff = null_vector(new_size)
+            self._length = self._vals.shape[0]
 
     cpdef void zeros(Vector self):
+        """
+        Overwrites the Vector with all zeros.
+        """
         cdef int i
         if self._length == 1:
             self._vals[0] = 0.
+            self._res_buff[0] = 0.
         else:
             for i in range(self._length):
                 self._vals[i] = 0.
+                self._res_buff[i] = 0.
 
     cpdef double get(Vector self, unsigned int index):
+        """
+        Retrieves the value at the given index.
+        
+        Args:
+            index (unsigned int): The index.
+
+        Returns:
+            The value at the index.
+        """
         if self._index_is_out_of_range(index):
             raise IndexError(
                 f"index ({index}) is out-of-range for length {self._length}."
@@ -256,6 +360,13 @@ cdef class Vector:
         return self._vals[index]
 
     cpdef void set(Vector self, unsigned int index, double value):
+        """
+        Sets the value at the given index.
+        
+        Args:
+            index (unsigned int): The index. 
+            value (double): The value to set.
+        """
         if self._index_is_out_of_range(index):
             raise IndexError(
                 f"index ({index}) is out-of-range for length {self._length}."
